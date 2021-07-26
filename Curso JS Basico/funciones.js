3df441fe419863bda73122259793240962957a3f
José Micó <80350702+josemicoo@users.noreply.github.com>
2021-07-26 02:36:02 -0300

    +

---
+++ Curso JS Basico/funciones.js
@@ -0,0 +1,20 @@
+/*Las funciones son las tareas que va a llevar a cabo el navegador. Existen 2 tipos de funciones
+1) Declarativas
+2) De expresión
+Ambas pueden llevar parámetros, que son los datos que necesitan para ejecutarse.
+Cada parámetro va separado por una coma.
+Cada instrucción que tenga la función debe terminar con ; .*/
+
+//Declarativas. FORMULA= function NOMBREFUNCION () {}
+
+function miFuncion () {
+    return 3;
+}
+
+
+//Expresión
+
+var miFuncion = function(){
+    return a + b;
+
+}
---
+++ Curso JS Basico/variables.js
@@ -0,0 +1,33 @@
+/*que son las variables y como utilizarla.*/
+
+
+/*VALOR DE NOMBRE PARA GUARDAR*/
+var nombre = "José";
+
+/*VAR=VARIBALE. Las variables sirven para guardar un valor.*/
+
+/*Hay 2 Estados de una VARIABLE: DECLARAR */
+
+var edad;
+
+edad
+
+/*Con el ejemplo de "var edad" estoy DECLARANDO una variable. Le digo al navegador que me reserve un espacio llamado "edad" que va a tener un valor. Una vez declarado vamos a INCIALIZAR esa variable*/
+
+//inicializar. La variable ya tiene un valor, esta declara y se puede utilizar.
+edad = 30;
+
+edad
+
+//otro ejemplo
+
+var elementos = ["Computadora", "Celular"];
+
+//varible tipo OBJETO. Es COMPLEJA xq guarda + valores de tipos diferentes
+
+var persona = {
+    nombre: "José"
+    edad: 19
+}
+
+persona 
